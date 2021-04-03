9841b7d367bebb31e8def460d3f5bd221133267d
Tulio Ferreira <tuliof210@gmail.com>
2021-04-02 23:10:24 -0300

    using cache response

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
 
-import { Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 
 import { HashService } from './hash.service';
 import { environment } from '../../environments/environment';
@@ -19,10 +19,30 @@ export class HttpService {
 
   constructor(private readonly http: HttpClient, private readonly hashService: HashService) {}
 
+  public responseCache = new Map();
+
   genericGet<T>(query: GenericQuery): Observable<T> {
-    const secret = this.hashService.createSecretHash();
-    const url = `${this.apiUrl}${query['endpoint']}?${secret}&${query['params']}`;
-    return this.http.get<T>(url).pipe(catchError(this.handleError));
+    const URL = query.params
+      ? `${this.apiUrl}${query['endpoint']}?${query['params']}`
+      : `${this.apiUrl}${query['endpoint']}`;
+    const dataFromCache = this.responseCache.get(URL);
+
+    if (dataFromCache) {
+      console.log('return from cache');
+      return of(dataFromCache);
+    } else {
+      console.log('return from API');
+      const secret = this.hashService.createSecretHash();
+      const FULL_URL = query.params ? `${URL}&${secret}` : `${URL}?${secret}`;
+
+      return this.http.get<T>(FULL_URL).pipe(
+        catchError(this.handleError),
+        map(data => {
+          this.responseCache.set(URL, data);
+          return data;
+        })
+      );
+    }
   }
 
   handleError(error: HttpErrorResponse) {
